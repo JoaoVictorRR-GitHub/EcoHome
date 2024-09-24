cd497c9f5b1d521f335d8383565e0562b38ee30b
JoaoVictorRR-GitHub <joaovictor.rr@hotmail.com>
2024-09-24 14:37:21 -0300

    Feature: Área para gerenciar o consumo de energia dos equipamentos no sistema.

---
+++ lib/Energia/area_consumo_energia.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+
+/// Área para visualizar dados sobre o consumo de energia dos equipamentos.
+class AreaConsumoEnergia extends StatefulWidget {
+  /// Área para visualizar dados sobre o consumo de energia dos equipamentos.
+  const AreaConsumoEnergia({super.key});
+
+  @override
+  State<AreaConsumoEnergia> createState() => _AreaConsumoEnergiaState();
+}
+
+class _AreaConsumoEnergiaState extends State<AreaConsumoEnergia> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
