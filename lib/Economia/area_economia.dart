f4afd367c2a8b3423c6ecb30a1cf5ab03843c2ce
JoaoVictorRR-GitHub <joaovictor.rr@hotmail.com>
2024-09-24 14:38:13 -0300

    Feature: Área para gerenciar os gastos com a energia numa casa.

---
+++ lib/Economia/area_economia.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+
+/// ###### Área para visualizar dados sobre os gastos com o consumo de energia dos equipamentos.
+class PaginaEconomia extends StatefulWidget {
+  /// ###### Área para visualizar dados sobre os gastos com o consumo de energia dos equipamentos.
+  const PaginaEconomia({super.key});
+
+  @override
+  State<PaginaEconomia> createState() => _PaginaEconomiaState();
+}
+
+class _PaginaEconomiaState extends State<PaginaEconomia> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Ahhhhhhhhhh!'),
+    );
+  }
+}
