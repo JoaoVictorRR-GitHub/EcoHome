54bbe69e03acf9dd3cb9ceeb120c404a5e08600a
JoaoVictorRR-GitHub <joaovictor.rr@hotmail.com>
2024-09-29 14:20:29 -0300

    Feature: Temporizador ligado ao provider para executar funções e atualizar cálculos e gráficos no sistema.

--- lib/Globais/funcoes.dart
+++ lib/Globais/funcoes.dart
@@ -1,5 +1,10 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
+
 import 'fontes_estilos.dart';
+import 'package:flutter/material.dart';
+import '../Economia/definicao_gasto.dart';
+import '../Equipamentos/definicao_equipamento.dart';
 
 
 
@@ -46,4 +51,47 @@ Future<void> mensagemAviso(BuildContext context, String titulo, String conteudo)
       );
     },
   );
+}
+
+
+/// ###### Classe de temporizador para executar funções no sistema.
+class TimerModel with ChangeNotifier {
+
+  Timer? timer; // Temporizador.
+
+  /// Método para simular o consumo de energia dos equipamentos.
+  void simularConsumo() {
+    const double baseConsumo = 6.0;         // Base do consumo de energia.
+    const double baseGeracao = 5.0;         // Base da geracao de energia.
+    const intervalo = Duration(seconds: 5); // Intervalo entre cada ciclo.
+
+    // Simulacao da geracao/cosumo de energia (W).
+    timer = Timer.periodic(intervalo, (Timer timer) {
+      if(equipamentos.isNotEmpty) {
+
+        // Todo: gerar para todos de uma vez?
+        Gasto gasto;                                            // Gasto com o consumo/geracao de energia (R$).
+        int tempoAtual = timer.tick * intervalo.inSeconds;      // Tempo total em segundos desde o início
+        int randIndex = Random().nextInt(equipamentos.length);  // Gera um indice aleatorio.
+
+        // Verificacao de equipamentos que geram energia mais não consomem.
+        if(equipamentos[randIndex].tipo == TipoEnum.painelSolar){
+          equipamentos[randIndex].geracaoEnergia = Random().nextDouble() * baseGeracao;
+          gasto = Gasto(-equipamentos[randIndex].geracaoEnergia, tempoAtual, intervalo.inSeconds);
+        } else{
+          equipamentos[randIndex].consumoEnergia = Random().nextDouble() * baseConsumo;
+          gasto = Gasto(equipamentos[randIndex].consumoEnergia, tempoAtual, intervalo.inSeconds);
+        }
+
+        gastos.add(gasto);  // Adiciona o gasto na lista.
+        notifyListeners();  // Atualiza o State das páginas para exibir a alteração nos graficos.
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
 }
