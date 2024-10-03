c667e306b82246cff2fd38cac4b44865dda2030b
JoaoVictorRR-GitHub <joaovictor.rr@hotmail.com>
2024-10-02 21:21:56 -0300

    Refactor: Reformulação da área da página inicial.

--- lib/pagina_home.dart
+++ lib/pagina_home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'Globais/fontes_estilos.dart';
 import 'package:eco_home/main.dart';
+import 'Globais/fontes_estilos.dart';
 
 
 
@@ -15,14 +15,14 @@ class Topicos {
 }
 
 /// ###### Lista de assuntos da pagina home.
-List<Topicos> listaAssuntos = [
-  Topicos(const MyHomePage(), 'SOBRE O APLICATIVO', const Icon(Icons.info_rounded, color: verdeThemeII)),
-  Topicos(const MyHomePage(), 'COMO USAR',          const Icon(Icons.app_shortcut_rounded, color: amareloTheme)),
-  Topicos(const MyHomePage(), 'CONFIGURAÇÕES',      const Icon(Icons.settings_applications_rounded, color: Colors.teal)),
-  Topicos(const MyHomePage(), 'Assunto', const Icon(Icons.abc_rounded, color: Colors.blue)),
-  Topicos(const MyHomePage(), 'Assunto', const Icon(Icons.abc_rounded, color: Colors.blue)),
-  Topicos(const MyHomePage(), 'Assunto', const Icon(Icons.abc_rounded, color: Colors.blue)),
-];
+// List<Topicos> listaAssuntos = [
+//   Topicos(const MyHomePage(), 'SOBRE O APLICATIVO', const Icon(Icons.info_rounded, color: verdeThemeII)),
+//   Topicos(const MyHomePage(), 'COMO USAR',          const Icon(Icons.app_shortcut_rounded, color: amareloTheme)),
+//   Topicos(const MyHomePage(), 'CONFIGURAÇÕES',      const Icon(Icons.settings_applications_rounded, color: Colors.teal)),
+//   Topicos(const MyHomePage(), 'Assunto', const Icon(Icons.abc_rounded, color: Colors.blue)),
+//   Topicos(const MyHomePage(), 'Assunto', const Icon(Icons.abc_rounded, color: Colors.blue)),
+//   Topicos(const MyHomePage(), 'Assunto', const Icon(Icons.abc_rounded, color: Colors.blue)),
+// ];
 
 
 
@@ -41,7 +41,74 @@ class _AreaHomeState extends State<AreaHome> {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        RichText(
+          textAlign: TextAlign.center,
+          text: const TextSpan(
+            children: [
+              WidgetSpan(child: Text('BEM VINDO', style: styleTextoSelecionado, textScaler: TextScaler.linear(3))),
+              WidgetSpan(child: Text('AO SEU ESPAÇO ECOHOME', style: styleTextoSelecionado, textScaler: TextScaler.linear(1.5))),
+            ],
+          ),
+        ),
+        InkWell(
+          highlightColor: verdeThemeI,  // Cor quando  o card eh pressionado.
+          customBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          onTap: () {
+            // Redirecionamento para a pagina do topico quando clicado.
+            Navigator.push(context, MaterialPageRoute(builder: (context) => const MyHomePage()));
+          },
+          child: const Card(
+            //elevation: 50,
+            shape: OutlineInputBorder(borderSide: BorderSide(color: verdeThemeI,  strokeAlign: 2, width: 2, style: BorderStyle.solid)),
+            color: kabulTheme,
+            child: Padding(
+              padding: EdgeInsets.all(30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.info_rounded, color: verdeThemeII),
+                  Padding(padding: EdgeInsets.only(top: 10)),
+                  Text('SOBRE O APLICATIVO', style: TextStyle(color: fundoColor, fontSize: 15, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
+                ],
+              ),
+            ),
+          ),
+        ),
+        // const Text(
+        //   '© 2024 JV Tech e Desenvolvimentos.\nTodos os direitos reservados.\n\n'
+        //       'Visite minha página GitHub:\nhttps://github.com/JoaoVictorRR-GitHub',
+        //   textAlign: TextAlign.center,
+        //   style: TextStyle(fontSize: 14, color: Colors.black54, height: 1.5),
+        // ),
+        GestureDetector(
+          onTap: () {},  // print("Link clicado!");
+          child: RichText(
+            textAlign: TextAlign.center,
+            text: const TextSpan(
+              style: TextStyle(fontSize: 14, color: Colors.black54, height: 1.5),
+              children: [
+                TextSpan(text: '© 2024 JV Tech e Desenvolvimentos.\nTodos os direitos reservados.\n\n'),
+                TextSpan(text: 'Visite minha página GitHub:\n'),
+                TextSpan(text: 'https://github.com/JoaoVictorRR-GitHub', style: TextStyle(fontSize: 14, color: Colors.blue, decoration: TextDecoration.underline)),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
+
+
+
+/*
+GridView.count(
       shrinkWrap: true,
       crossAxisCount: 2,
       mainAxisSpacing: 20,
@@ -77,5 +144,4 @@ class _AreaHomeState extends State<AreaHome> {
         },
       ),
     );
-  }
-}
+* */
