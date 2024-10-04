bd20ddd1417917687c4aff28d91d8154ea9b0e53
JoaoVictorRR-GitHub <joaovictor.rr@hotmail.com>
2024-10-04 12:53:47 -0300

    Refactor: Mudanças no design e nas cores da aplicação, modularização do título e modularização da área de navegação entre as páginas.

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'EcoHome',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -48,13 +47,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  var _paginaIndex = 0;
-  final List _paginas = [
-    {'pagina': const AreaHome(),            'titulo': "INÍCIO",              'corTema': fundoColor},
+  /// Indice da área selecionada.
+  var _areaIndex = 0;
+
+  /// Lista do conteúdo de cada área.
+  final List _conteudoAreas = [
+    {'pagina': const AreaHome(),            'titulo': "INÍCIO",              'corTema': begeClaro},
     {'pagina': const AreaConsumoEnergia(),  'titulo': "CONSUMO DE ENERGIA",  'corTema': verdeThemeII},
-    {'pagina': const PaginaEconomia(),      'titulo': "ECONOMIA",            'corTema': laranjaTheme},
-    {'pagina': const AreaEquipamentos(),    'titulo': "EQUIPAMENTOS",        'corTema': kabulTheme},
+    {'pagina': const AreaEconomia(),        'titulo': "ECONOMIA",            'corTema': fundoColor},
+    {'pagina': const AreaEquipamentos(),    'titulo': "EQUIPAMENTOS",        'corTema': begeClaro},
   ];
+
 
   @override
   void initState() {
@@ -68,6 +71,63 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
+  /// Método para desenhar o área do título de cada área da aplicação.
+  PreferredSize areaTitulo(){
+    double altura = 50; // MediaQuery.of(context).size.height * 0.06,
+
+    return PreferredSize(
+      preferredSize: Size(MediaQuery.of(context).size.width, altura),
+      child: Container(
+        height: altura,
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          color:        headerColor,
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(10)),
+          boxShadow:    [
+            BoxShadow(color: verdeThemeI, offset: offsetsIII, spreadRadius: 3),
+            BoxShadow(color: verdeThemeI, offset: offsetsIV, spreadRadius: 2),
+          ],
+        ),
+        child: Text(
+          _conteudoAreas[_areaIndex]["titulo"], // Titulo de cada area.
+          style: styleTextoSelecionado.copyWith(fontSize: 20),
+        ),
+      ),
+    );
+  }
+
+  /// Método para desenhar a parte de navegação entre as áreas da aplicação.
+  Container areaNavegacao(){
+    return Container(
+      height: 70,
+      decoration: const BoxDecoration(
+        color:        headerColor,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
+        border:       Border(top: BorderSide(width: 3, color: verdeThemeI)),
+        boxShadow:    [BoxShadow(color: verdeThemeI, offset: offsetsIV, spreadRadius: 3)],
+      ),
+      child: BottomNavigationBar(
+        elevation: 15,
+        onTap: (index) {
+          setState(() { _areaIndex = index; });
+        },
+        currentIndex: _areaIndex,
+        showUnselectedLabels: true,                       // Indica se o texto dos itens não selecionados sera exibido.
+        useLegacyColorScheme: false,                      // Indica se as cores padrao serao utilizadas.
+        backgroundColor: headerColor,                     // Nota: So funciona com type fixed.
+        type: BottomNavigationBarType.fixed,              // Estilo dos itens.
+        selectedLabelStyle:   styleTextoSelecionado2,     // Estilo da fonte do texto no tab selecionado.
+        unselectedLabelStyle: styleTextoNaoSelecionado2,  // Estilo da fonte do texto nos tabs nao selecionados.
+        items: const [  // Areas da aplicacao.
+          BottomNavigationBarItem(backgroundColor: Colors.red, label: "HOME",         icon: Icon(Icons.house_rounded,               color: verdeThemeII,  shadows: [Shadow(color: Colors.black54, offset: offsetsI)])),
+          BottomNavigationBarItem(backgroundColor: headerColor, label: "CONSUMO",      icon: Icon(Icons.energy_savings_leaf_rounded, color: verdeThemeI,   shadows: [Shadow(color: amareloTheme,   offset: offsetsI)])),
+          BottomNavigationBarItem(backgroundColor: headerColor, label: "ECONOMIA",     icon: Icon(Icons.eco_rounded,                 color: laranjaTheme,  shadows: [Shadow(color: Colors.black,   offset: offsetsI)])),
+          BottomNavigationBarItem(backgroundColor: headerColor, label: "EQUIPAMENTO",  icon: Icon(Icons.control_camera_rounded,      color: Colors.white,  shadows: [Shadow(color: verdeThemeI,    offset: offsetsI)])),
+        ],
+      ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -78,88 +138,35 @@ class _MyHomePageState extends State<MyHomePage> {
         result = didPop;
         if(didPop && context.mounted) Navigator.pop(context);
       },
-      child: DefaultTabController(
-        initialIndex: 0,
-        length: _paginas.length,
-        child: Scaffold(
-          appBar: AppBar(
-            elevation: 10,
-            toolbarHeight: 90,  // Altura da area do titulo.
-            centerTitle: true,
-            leadingWidth: 100,  // Largura do logo.
-            shadowColor: verdeThemeI,
-            backgroundColor: headerColor,
-            leading: Image.asset('imagens/Logo - Sem Fundo.png'),
-            title: RichText(
-              //textAlign: TextAlign.center,
-              textScaler: const TextScaler.linear(0.7),
-              text: const TextSpan(
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, letterSpacing: 2, height: 1.5),
-                children: [
-                  TextSpan(text: 'Eco',   style: TextStyle(color: Colors.white, shadows: [Shadow(color: verdeThemeI,  offset: offsetsII)])),
-                  TextSpan(text: 'Home',  style: TextStyle(color: verdeThemeI,  shadows: [Shadow(color: fundoColor,   offset: offsetsII)])),
-                  TextSpan(text: '\nVocê no controle da sua casa\n', style: TextStyle(color: Colors.black, fontSize: 15)),
-                ],
-              ),
-            ),
-          ),
-          backgroundColor: _paginas[_paginaIndex]['corTema'],
-          body: TabBarView(
-            children: List.generate(_paginas.length, (index) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    alignment: Alignment.center,
-                    height: MediaQuery.of(context).size.height * 0.06,
-                    decoration: const BoxDecoration(
-                      color:        headerColor,
-                      borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
-                      boxShadow:    [BoxShadow(color: verdeThemeI, offset: Offset(0, 3), spreadRadius: 3)],
-                    ),
-                    child: Text(
-                      _paginas[index]["titulo"],
-                      style: const TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold, shadows: [Shadow(color: verdeThemeII, offset: offsetsI)]),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.1, vertical: MediaQuery.of(context).size.height * 0.1),
-                      child: _paginas[index]['pagina'],
-                    ),
-                  ),
-                ],
-              );
-            }),
-          ),
-          bottomNavigationBar: Container(
-            decoration: const BoxDecoration(
-              color:        headerColor,
-              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-              boxShadow:    [BoxShadow(color: verdeThemeI, offset: Offset(0, -3), spreadRadius: 3)],
-            ),
-            child: TabBar(
-              onTap: (value) {
-                setState(() { _paginaIndex = value; });
-              },
-              tabAlignment: TabAlignment.fill,
-              labelPadding: const EdgeInsets.all(1),
-              labelStyle:   styleTextoSelecionado,            // Estilo da fonte do texto no tab selecionado.
-              textScaler:   const TextScaler.linear(0.7),     // Escala do texto.
-              unselectedLabelStyle: styleTextoNaoSelecionado, // Estilo da fonte do texto nos tabs nao selecionados.
-              indicatorSize:    TabBarIndicatorSize.tab,      // Largura do indicador no tab selecionado.
-              indicatorPadding: const EdgeInsets.only(top: 40),
-              indicator:        const BoxDecoration(color: kabulTheme, borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
-              tabs: const [
-                Tab(text: "HOME",         icon: Icon(Icons.house_rounded,               color: verdeThemeII,  shadows: [Shadow(color: Colors.black54, offset: offsetsI)]), iconMargin: EdgeInsets.only(bottom: 10)),
-                Tab(text: "CONSUMO",      icon: Icon(Icons.energy_savings_leaf_rounded, color: verdeThemeI,   shadows: [Shadow(color: amareloTheme,   offset: offsetsI)]), iconMargin: EdgeInsets.only(bottom: 10)),
-                Tab(text: "ECONOMIA",     icon: Icon(Icons.eco_rounded,                 color: laranjaTheme,  shadows: [Shadow(color: Colors.black,   offset: offsetsI)]), iconMargin: EdgeInsets.only(bottom: 10)),
-                Tab(text: "EQUIPAMENTO",  icon: Icon(Icons.control_camera_rounded,      color: Colors.white,  shadows: [Shadow(color: verdeThemeI,    offset: offsetsI)]), iconMargin: EdgeInsets.only(bottom: 10)),
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 15,
+          toolbarHeight: 80,  // Altura da area do titulo.
+          centerTitle: true,
+          leadingWidth: 100,  // Largura do logo.
+          shadowColor: amareloDourado,
+          backgroundColor: headerColor,
+          leading: Image.asset('imagens/Logo - Sem Fundo.png'),
+          title: RichText(
+            //textAlign: TextAlign.center,
+            textScaler: const TextScaler.linear(0.7),
+            text: const TextSpan(
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, letterSpacing: 2, height: 1.5),
+              children: [
+                TextSpan(text: 'Eco',   style: TextStyle(color: Colors.white, shadows: [Shadow(color: verdeThemeI,  offset: offsetsII)])),
+                TextSpan(text: 'Home',  style: TextStyle(color: verdeThemeI,  shadows: [Shadow(color: fundoColor,   offset: offsetsII)])),
+                TextSpan(text: '\nVocê no controle da sua casa\n', style: styleTextoNormal),
               ],
             ),
           ),
+          bottom: areaTitulo(), // Area do  titutlo de cada pagina.
         ),
+        backgroundColor: _conteudoAreas[_areaIndex]['corTema'], // Cor do fundo de cada area.
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.1, vertical: MediaQuery.of(context).size.height * 0.1),
+          child: _conteudoAreas[_areaIndex]['pagina'],  // Conteudo de cada area.
+        ),
+        bottomNavigationBar: areaNavegacao(), // Navegacao entre as areas.
       ),
     );
   }
